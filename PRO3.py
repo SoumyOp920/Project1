840b94ceced8a8897b11913cf8621b7c0ea12b52
SoumyOp920 <soumymittal09@gmail.com>
2025-04-13 14:35:49 +0530

    Add files via upload

---
+++ PRO3.py
@@ -0,0 +1,5 @@
+a=int(input("Enter an integer: "))
+if a%2==0:
+    print(a,"is an EVEN integer")
+else:
+    print(a,"is an ODD integer")
---
+++ PRO4.py
@@ -0,0 +1,4 @@
+SUM=0
+for i in range(1,51):
+    SUM+=i
+print("The sum of number from 1 to 50 is:",SUM)
